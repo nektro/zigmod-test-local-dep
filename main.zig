33989c87bbb17d99711109e46a29f4de01022427
Meghan Denny <hello@nektro.net>
2026-06-01 21:08:58 -0700

    update to zig 0.15.2

--- main.zig
+++ main.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const pkg = @import("add");
 
 test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
+    var list = std.array_list.Managed(i32).init(std.testing.allocator);
     defer list.deinit();
     try list.append(42);
     try std.testing.expectEqual(@as(i32, 42), list.pop());
